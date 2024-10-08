4f4bfa21db467e703f215d69366697f2cac62199
p-k-pavan <pkpavan2003@email.com>
2024-10-08 14:52:14 +0530

    Resume Model

---
+++ Api/models/resume.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const resumeSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Resume = mongoose.model("resume", resumeSchema);
+
+export default Resume;
